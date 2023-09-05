a82aa72bf23da5b4ebe956a80647c1fa8f315130
Audace <adenikeaudace@gmail.com>
2023-09-05 11:25:52 +0100

    la validation  est bonne

--- src/routes/route3.js
+++ src/routes/route3.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const textcontroller = require("../controller/textcontroller");
+import { getOneText,createText } from "../service/textservice";
 
 const router3 = express.Router();
-router3.get("/text", textcontroller.getOneText);
+router3.get("/text", getOneText);
 
 module.exports = router3;
